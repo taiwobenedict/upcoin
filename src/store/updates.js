d79232da3a36a08512b8d76965f64a013984fdd7
taiwobenedict <ajayitaiwotunta@gmail.com>
2023-11-28 21:11:09 +0100

    updated

--- src/store/updates.js
+++ src/store/updates.js
@@ -1,6 +1,6 @@
 const updates = [
     {
-        heading: "Lorem, ipsum.",
+        heading: "Platform Update v2.1.0",
         caption: " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, magni.",
         body: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus unde atque fugit, illo, eaque, quod alias in nesciunt doloribus et earum deserunt aspernatur animi! Dignissimos, laborum? Illum corrupti eos dolor.",
     }
